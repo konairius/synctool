2418bba75825c116d753b840630f5450e2e43fc7
konsti <rennerkonsti@gmail.com>
2014-01-06 10:34:27 +0100

    Construction methods now automatically add the new object to the session and flush so all id fields should be filled

--- base.py
+++ base.py
@@ -4,17 +4,44 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, UniqueConstraint
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base, declared_attr
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, UniqueConstraint, create_engine
+from sqlalchemy.orm import relationship, backref, sessionmaker
 
 Base = declarative_base()
 
+global_session = None
 
-class Host(Base):
-    __tablename__ = 'hosts'
+
+def session():
+    global global_session
+    if global_session is None:
+        logger.warning('Database Session not set: Creating Dummy Session!')
+        engine = create_engine('sqlite:///:memory:', echo=False)
+        Base.metadata.create_all(engine)
+        s = sessionmaker(bind=engine)
+        global_session = s()
+    return global_session
+
+
+def set_session(new_session):
+    logger.info('Database Session set to: %s' % new_session.get_bind())
+    global global_session
+    global_session = new_session
+
+
+class DBObject(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
 
     id = Column(Integer, primary_key=True)
+
+
+class Host(Base, DBObject):
     name = Column(String, nullable=False, unique=True)
 
     root = relationship('Folder', primaryjoin="and_(Host.id==Folder.host_id, Folder.name=='<root>')", uselist=False,
@@ -35,15 +62,21 @@ class Host(Base):
 
         if not self.root is None:
             raise RuntimeError('%r already has a root' % self)
-        self.root = Folder(name='<root>', host=self)
+        root = Folder(name='<root>', host=self)
+        session().add(root)
+        session().flush()
+        self.root = root
         return self.root
 
     def add_queue(self):
-        return Queue(host_id=self.id)
+        queue = Queue(host_id=self.id)
+        session().add(queue)
+        session().flush()
+
+        return queue
 
 
-class Folder(Base):
-    __tablename__ = 'folders'
+class Folder(Base, DBObject):
     __table_args__ = (
         UniqueConstraint('parent_id', 'name'),
     )
@@ -51,10 +84,10 @@ class Folder(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
 
-    host_id = Column(Integer, ForeignKey('hosts.id'), nullable=False)
+    host_id = Column(Integer, ForeignKey('host.id'), nullable=False)
     host = relationship('Host', cascade='all')
 
-    parent_id = Column(Integer, ForeignKey('folders.id'))
+    parent_id = Column(Integer, ForeignKey('folder.id'))
     parent = relationship('Folder', remote_side=[id, host_id], backref='folders', cascade='all')
 
     @property
@@ -74,14 +107,19 @@ class Folder(Base):
         return '<Folder(%s)>' % self.uri
 
     def add_folder(self, name):
-        return Folder(parent=self, name=name, host=self.host)
+        folder = Folder(parent=self, name=name, host=self.host)
+        session().add(folder)
+        session().flush()
+        return folder
 
     def add_file(self, name):
-        return File(folder=self, name=name, host=self.host)
+        file = File(folder=self, name=name, host=self.host)
+        session().add(file)
+        session().flush()
+        return file
 
 
-class File(Base):
-    __tablename__ = 'files'
+class File(Base, DBObject):
     __table_args__ = (
         UniqueConstraint('folder_id', 'name'),
     )
@@ -91,10 +129,10 @@ class File(Base):
     hash = Column(String)
     mtime = Column(DateTime)
     size = Column(Integer)
-    host_id = Column(Integer, ForeignKey('hosts.id'), nullable=False)
+    host_id = Column(Integer, ForeignKey('host.id'), nullable=False)
     host = relationship('Host', cascade='all')
 
-    folder_id = Column(Integer, ForeignKey('folders.id'), nullable=False)
+    folder_id = Column(Integer, ForeignKey('folder.id'), nullable=False)
     folder = relationship('Folder', backref=backref('files'), cascade='all')
 
     @property
@@ -109,18 +147,14 @@ class File(Base):
         return '<File(%s)>' % self.uri
 
 
-class Job(Base):
-    __tablename__ = 'jobs'
-
-    id = Column(Integer, primary_key=True)
-
-    source_id = Column(Integer, ForeignKey('folders.id'), nullable=False)
+class Job(Base, DBObject):
+    source_id = Column(Integer, ForeignKey('folder.id'), nullable=False)
     source = relationship('Folder', foreign_keys=[source_id])
 
-    target_id = Column(Integer, ForeignKey('folders.id'), nullable=False)
+    target_id = Column(Integer, ForeignKey('folder.id'), nullable=False)
     target = relationship('Folder', foreign_keys=[target_id])
 
-    queue_id = Column(Integer, ForeignKey('queues.id'), nullable=False)
+    queue_id = Column(Integer, ForeignKey('queue.id'), nullable=False)
     queue = relationship('Queue', backref=backref('jobs'))
 
     def __str__(self):
@@ -130,12 +164,8 @@ class Job(Base):
         return '<Job(%s ==> %s)(id=%s, queue_id=%s)>' % (self.source.uri, self.target.uri, self.id, self.queue_id)
 
 
-class Queue(Base):
-    __tablename__ = 'queues'
-
-    id = Column(Integer, primary_key=True)
-
-    host_id = Column(Integer, ForeignKey('hosts.id'), nullable=False)
+class Queue(Base, DBObject):
+    host_id = Column(Integer, ForeignKey('host.id'), nullable=False)
     host = relationship('Host', backref=backref('queues'))
 
     def __repr__(self):
--- test.py
+++ test.py
@@ -10,7 +10,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import IntegrityError
 
-from base import File, Host, Base
+from base import File, Host, Base, set_session
 
 #Setup test database
 database = create_engine('sqlite:///test.sqlite', echo=False)
@@ -24,19 +24,18 @@ class FileTest(unittest.TestCase):
 
     def test_create(self):
         session = Session()
+        set_session(session)
         host = Host(name='localhost')
-        session.add(host)
         root = host.new_root()
+        self.assertIsNotNone(root)
         self.assertRaises(RuntimeError, host.new_root)
         dev = root.add_folder('dev')
         file = dev.add_file('null')
         file2 = session.query(File).filter_by(folder=dev, name='null').first()
 
         self.assertEqual(file, file2)
-        session.commit()
 
-        dev.add_file('null')
-        self.assertRaises(IntegrityError, session.commit)
+        self.assertRaises(IntegrityError, dev.add_file, 'null')
 
 
 class TestJobs(unittest.TestCase):
@@ -44,24 +43,20 @@ class TestJobs(unittest.TestCase):
         Base.metadata.drop_all(database)
         Base.metadata.create_all(database)
         self.session = Session()
+        set_session(self.session)
         self.host = Host(name='localhost')
         self.session.add(self.host)
         self.session.flush()
         self.queue = self.host.add_queue()
-        self.session.add(self.queue)
         self.root = self.host.new_root()
         self.source = self.root.add_folder('source')
         self.target = self.root.add_folder('target')
-        self.session.flush()
 
     def test_add_job(self):
         job = self.queue.add_job(source=self.source, target=self.target)
-        self.session.add(job)
-        self.session.flush()
-        self.session.commit()
 
     def tearDown(self):
-        pass
+        self.session.commit()
 
 
 if __name__ == '__main__':
