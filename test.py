cf65820b380db03b09c69c366ab03af4937a62e7
konsti <rennerkonsti@gmail.com>
2014-01-06 08:33:26 +0100

    Added Test for job and Queue

--- test.py
+++ test.py
@@ -10,14 +10,15 @@ from sqlalchemy.orm import sessionmaker
 from base import File, Host, Base
 
 #Setup test database
-database = create_engine('sqlite:///test.sqlite', echo=True)
+database = create_engine('sqlite:///test.sqlite', echo=False)
 Session = sessionmaker(bind=database)
-
-Base.metadata.drop_all(database)
-Base.metadata.create_all(database)
 
 
 class FileTest(unittest.TestCase):
+    def setUp(self):
+        Base.metadata.drop_all(database)
+        Base.metadata.create_all(database)
+
     def test_create(self):
         session = Session()
         host = Host(name='localhost')
@@ -33,6 +34,31 @@ class FileTest(unittest.TestCase):
 
         dev.add_file('null')
         self.assertRaises(IntegrityError, session.commit)
+
+
+class TestJobs(unittest.TestCase):
+    def setUp(self):
+        Base.metadata.drop_all(database)
+        Base.metadata.create_all(database)
+        self.session = Session()
+        self.host = Host(name='localhost')
+        self.session.add(self.host)
+        self.session.flush()
+        self.queue = self.host.add_queue()
+        self.session.add(self.queue)
+        self.root = self.host.new_root()
+        self.source = self.root.add_folder('source')
+        self.target = self.root.add_folder('target')
+        self.session.flush()
+
+    def test_add_job(self):
+        job = self.queue.add_job(source=self.source, target=self.target)
+        self.session.add(job)
+        self.session.flush()
+        self.session.commit()
+
+    def tearDown(self):
+        pass
 
 
 if __name__ == '__main__':
