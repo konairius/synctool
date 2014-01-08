c269705f3a3bf28c92085ed4e8815b965fa57833
Konstantin Renner <rennerkonsti@gmail.com>
2014-01-08 11:43:46 +0100

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='UranosBase',
+    version='0.1',
+    packages=[''],
+    url='nerdcave.eu',
+    license='BSD',
+    author='konsti',
+    author_email='rennerkonsti@gmail.com',
+    description='File-sync-tool', requires=['sqlalchemy']
+)
