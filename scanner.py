9c34ce2aabc0b13cd21ae724c33d3e02cf1db17e
Konstantin Renner <rennerkonsti@gmail.com>
2014-01-09 13:29:10 +0100

    Working implementation

--- scanner.py
+++ scanner.py
@@ -4,7 +4,6 @@
 This is the scanner daemon, basically just a filesystem walker that checks if the database is up-to-date
 and requests others to do the updates if necessary.
 """
-from _md5 import md5
 
 import argparse
 from datetime import datetime
@@ -13,9 +12,13 @@ from os import listdir
 from os.path import join
 import socket
 from time import sleep
+import sys
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from base import set_session, Base, Folder, Host
+
+from base import set_session, Host, HashRequest, session
+
 
 __author__ = 'konsti'
 
@@ -25,6 +28,11 @@ logger = logging.getLogger(__name__)
 
 
 def scan(folder):
+    """
+    Main scanner function
+    @param folder: the folder you want to scan
+    @raise AttributeError: if folder is not local
+    """
     if not folder.host.is_local:
         raise AttributeError('%r is not local' % folder)
 
@@ -36,71 +44,73 @@ def scan(folder):
                     path):
                 if archive is not None:
                     archive.delete()
-                folder.add_file(name=name, fhash=_calculate_hash(path), mtime=datetime.fromtimestamp(getmtime(path)),
-                                size=getsize(path))
+                request_hash(name=name, folder=folder, mtime=datetime.fromtimestamp(getmtime(path)),
+                             size=getsize(path))
         elif isdir(path):
             if archive is None:
                 archive = folder.add_folder(name)
             scan(archive)
 
+    for file in folder.files:
+        if not isfile(file.path):
+            file.delete()
 
-def _calculate_hash(file_path: str):
-    logger.info('Calculating hash for: %s ' % file_path)
-    _hash = md5()
-    with open(file_path, 'rb') as data:
-        for chunk in iter(lambda: data.read(1024 * _hash.block_size), b''):
-            _hash.update(chunk)
-        value = _hash.hexdigest()
-        return value
+    for child in folder.folders:
+        if not isdir(child.path):
+            child.delete()
 
 
-def daemon(session):
-    set_session(session)
+def request_hash(name, folder, mtime, size):
+    """
+    Creates a HashRequest for the given file
+    @param name: The filename
+    @param folder: The parent Folder
+    @param mtime: timestamp as a datetime.datetime object
+    @param size: Filesize as Integer in Bytes
+    """
+    request = session().query(HashRequest).filter(HashRequest.name == name, HashRequest.folder == folder,
+                                                  HashRequest.size == size, HashRequest.mtime == mtime)
+    if request is None:
+        request = HashRequest(name=name, folder=folder, mtime=mtime, size=size, host=folder.host)
+        session().add(request)
+        session().flush()
+
+
+def daemon(interval):
+    """
+    @param interval: Integer, seconds between to scan runs
+    """
     host = Host.by_name(socket.gethostname())
     while True:
+        logger.debug('Stating scanner round')
         for root in host.roots:
             scan(root)
-            session.commit()
-        sleep(3600)
+            session().commit()
+        sleep(interval)
 
 
-def main():
-    parser = argparse.ArgumentParser(description='Filesystem Scanner')
-    parser.add_argument('-d', '--database', type=str, metavar='"Connection String"', default='sqlite:///:memory:')
-    parser.add_argument('--dir', dest='dirs', type=str, metavar='URI',
-                        help='The directory you want to add to the Database', action='append')
+def main(args=sys.argv[1:]):
+    """
+    main :)
+    @param args: the args used for the parser, just useful for testing
+    """
+    parser = argparse.ArgumentParser(description='Scanner Tool')
+    parser.add_argument('-d', '--database', type=str, metavar='"Connection String"', required=True)
     parser.add_argument('--debug', action='store_true')
-    parser.add_argument('--create_schema', action='store_true')
-    parser.add_argument('--config_only', action='store_true')
-    args = parser.parse_args()
+    parser.add_argument('-i', '--interval', type=int, metavar='SECONDS', default=360,
+                        help='Interval between two Scan runs, defaults to 1 hour')
+    args = parser.parse_args(args)
     if args.debug:
         logging.basicConfig(level=logging.DEBUG)
+        logger.debug('Configurator started with following arguments: %s' % args)
     else:
         logging.basicConfig(level=logging.INFO)
 
     database = create_engine(args.database, echo=False)
-
-    if args.create_schema:
-        Base.metadata.create_all(database)
-
-    session = sessionmaker(bind=database)()
-    set_session(session)
-    print(args.dirs)
-    for root_dir in args.dirs:
-        root_uri = '%s::%s' % (socket.gethostname(), root_dir)
-        print(root_uri)
-        Folder.by_uri(root_uri)
-
-    session.commit()
-
-    if args.config_only:
-        exit(0)
-
-    daemon(session)
-
-    session.commit()
-    session.close_all()
+    s = sessionmaker(bind=database)()
+    set_session(s)
+    daemon(args.interval)
 
 
 if __name__ == '__main__':
-    main()
+    exit(main())
