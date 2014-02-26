b2b0ba061ae2817b48c52b9b3739cfe93e459fa5
Konstantin Renner <rennerkonsti@gmail.com>
2014-02-27 00:49:37 +0100

    Just a Dummy Implementation for Testing

--- config.py
+++ config.py
@@ -3,15 +3,19 @@
 This module contains the static methods for parsing the XML Configfile
 """
 from functools import partial
-from base import Folder
-import hasher
+
+from base import Host
 import scanner
+import database
+
 
 __author__ = 'konsti'
 
 import logging
 
 logger = logging.getLogger(__name__)
+
+HOSTNAME = 'konsti-desktop'
 
 
 def get_roles(configfile):
@@ -22,11 +26,16 @@ def get_roles(configfile):
     #TODO: Dummy Implementation
 
     roles = list()
-    roles.append(partial(hasher.run, 3, 120))
+    #roles.append(partial(hasher.run, 3, 120))
 
+    session = database.get_global_session()
 
-    folder = Folder
-    roles.append(partial(scanner.run, 3, 120))
+    host = Host.by_name('konsti-desktop', database.get_global_session())
+    #folder = host.add_root('/media/konsti/Storage/makeMKV', database.get_global_session())
+    folder = host.add_root('/home/konsti/Code', database.get_global_session())
+    session.add(host)
+    session.add(folder)
+    roles.append(partial(scanner.run, folder, 15))
 
     print(roles)
 
