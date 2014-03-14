25211cd32d82c88022b3f3cd2844f8024c89646a
Konstantin Renner <rennerkonsti@gmail.com>
2014-03-14 07:45:05 +0100

    YAMR (Yet Another Major Refactoring)

--- scanner.py
+++ scanner.py
@@ -5,118 +5,159 @@ This is the scanner daemon, basically just a filesystem walker that checks if th
 and requests others to do the updates if necessary.
 """
 
-import asyncio
-from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
+from queue import Queue
+from threading import Thread
 import time
-from functools import partial
 import logging
 from os import listdir
 from pathlib import Path
 
-from base import HashRequest, Folder, File, FilesystemObject
+from sqlalchemy.orm.exc import NoResultFound
+
+from base import Folder, FilesystemObject
 import config
 import database
+import database_logging
 
 
 __author__ = 'konsti'
 
 logger = logging.getLogger(__name__)
 
-WALKER_POOL = None
 
-
-@asyncio.coroutine
-def add_child(path: Path):
+class Worker(object):
     """
-    Adds a Directory to the Database
-    @param path: a pathlib Path object
-    """
-    logger.info('Adding Item: [%r]' % path)
-    session = database.get_global_session()
-    parent = yield from Folder.by_uri('%s::%s' % (config.HOSTNAME, path.parent), session)
-
-    if path.is_dir():
-        child = parent.add_folder(path.name)
-        yield from walk(child)
-    elif path.is_file():
-        child = parent.add_file(name=path.name, mtime=datetime.fromtimestamp(path.stat().st_mtime),
-                                size=path.stat().st_size, fhash=None)
-    else:
-        raise RuntimeError('%s is neither File nor Folder' % path)
-    session.add(child)
-    return child
-
-
-@asyncio.coroutine
-def check_item(path: Path):
-    """
-    Checks if an Filesystem Object matches it's Database entry
-    @param path: a pathlib Path object
-    @raise RuntimeError: if the object is not in the database
+    scans a directory consumed from queue
     """
 
-    logger.debug('Checking Item: [%r]' % path)
-    session = database.get_global_session()
-    db_item = yield from FilesystemObject.by_uri('%s::%s' % (config.HOSTNAME, path), session)
-    if path.is_dir() and isinstance(db_item, Folder):
-        adds, checks = yield from walk(db_item)
-        return checks
-    if path.is_file() and isinstance(db_item, (File, HashRequest)):
-        if path.stat().st_size == db_item.size and datetime.fromtimestamp(path.stat().st_mtime) == db_item.mtime:
-            return True, db_item
-    logger.info('Check failed on [%r]' % db_item)
-    return False, db_item
+    def __init__(self, queue: Queue):
+        """
+        @param queue: The queue new consumed are consumed from
+        """
+        self._queue = queue
+        self._thread = Thread(target=self._loop)
+        self._thread.daemon = True
 
+        self._thread.start()
+        logging.debug('Worker Started: %s' % self._thread.name)
 
-@asyncio.coroutine
-def walk(root):
-    """
-    @param root: a Folder
-    @return:
-    """
-    adds = list()
-    checks = list()
-    babes = list()
-    logger.debug('Filesystem walker entered %s' % root.path)
-    for child_name in listdir(root.path):
-        babes.append((
-            asyncio.Task(root.child_by_name(child_name, database.get_global_session())), Path(root.path) / child_name))
-    for child, child_path in babes:
-        yield from asyncio.wait_for(child, None)
-        child = child.result()
-        if child is None:
-            if child_path.is_dir() or child_path.is_file():
-                adds.append(asyncio.Task(add_child(child_path)))
-            else:
-                logger.warn(
-                    'Ignored Filesystem Object: [%r] because it is neither file nor directory.' % child_path)
+    def _loop(self):
+        """
+        Executes the daemon loop
+        """
+        while True:
+            item = self._queue.get(block=True)
+            try:
+                self._scan(item)
+            except Exception as e:
+                logging.exception(e)
+                time.sleep(30)
+            finally:
+                self._queue.task_done()
+
+    def _scan(self, item: Path):
+        """
+        @param item: a File or a Folder
+        """
+        if item.is_dir():
+            self._scan_folder(item)
+        elif item.is_file():
+            self._scan_file(item)
+
+    def _scan_file(self, file: Path):
+        """
+        @param file: a File
+        """
+
+        logger.debug('Scanning File: %s' % file)
+        session = database.get_session(new_engine=False)
+        try:
+            db_item = FilesystemObject.by_uri('%s::%s' % (config.HOSTNAME, file), session)
+        except NoResultFound:
+            self._add_child(file)
+            session.close()
+            return
+
+        if file.stat().st_size == db_item.size and datetime.fromtimestamp(file.stat().st_mtime) == db_item.mtime:
+            pass
         else:
-            checks.append(asyncio.Task(check_item(child_path)))
+            session.delete(db_item)
+            session.commit()
+            self._queue.put(file)
+            logging.info('Cache mismatch: %r' % db_item)
+        session.close()
 
-    if len(adds) > 0:
-        yield from asyncio.wait(adds)
-    if len(checks) > 0:
-        yield from asyncio.wait(checks)
+    def _scan_folder(self, folder: Path):
+        """
+        @param folder: a Folder
+        """
+        logger.debug('Scanning Folder: %s' % folder)
+        session = database.get_session(new_engine=False)
+        try:
+            FilesystemObject.by_uri('%s::%s' % (config.HOSTNAME, folder), session)
+        except NoResultFound:
+            self._add_child(folder)
+        finally:
+            session.close()
 
-    logger.debug('Filesystem walker completed %s' % root.path)
-    return adds, checks
+        for child_name in listdir(str(folder)):
+            self._queue.put(folder / child_name)
+
+    def _add_child(self, path: Path):
+        """
+        Adds a Directory to the Database
+        @param path: a pathlib Path object
+        """
+        logger.info('Adding Item: [%r]' % path)
+        session = database.get_session(new_engine=False)
+        parent = Folder.by_uri('%s::%s' % (config.HOSTNAME, path.parent), session)
+        if path.is_dir():
+            child = parent.add_folder(path.name)
+            self._queue.put(path)
+        elif path.is_file():
+            child = parent.add_file(name=path.name, mtime=datetime.fromtimestamp(path.stat().st_mtime),
+                                    size=path.stat().st_size, fhash=None)
+        else:
+            session.close()
+            raise RuntimeError('%s is neither File nor Folder' % path)
+        session.add(child)
+        session.commit()
+        session.close()
 
 
-def run(folder, interval, future=None):
+def run(folder, interval):
     """
-    The runner for the asyncio framework
-    @param future: is used to determine if the function was called as result of a callback
     @param folder: The Folder that should be Scanned
     @param interval: The interval in which it should be scanned, None if only once, else the time in seconds
     """
-    #TODO: Make it use the eventloop sleep
-    global WALKER_POOL
-    WALKER_POOL = ThreadPoolExecutor(max_workers=4)
-    runner = partial(run, folder, interval)
-    if future and interval:
+
+    database_logging.configure_logger()
+    logging.info('Scanner started with: folder=%s, interval=%s' % (folder, interval))
+
+    folder = Path(folder.path)
+
+    dir_queue = Queue()
+    init(workers=40, queue=dir_queue)
+
+    while True:
+        start = datetime.now()
+        if dir_queue.empty():  # Only add the Root folder if the Queue is empty.
+            dir_queue.put(folder)
+        dir_queue.join()
+        logging.debug('Scanner round Completed in %s' % (datetime.now() - start))
+        if interval is None:
+            break  # This is used for debugging
         time.sleep(interval)
-    future = asyncio.async(walk(folder))
-    if not interval:
-        return
-    future.add_done_callback(runner)
+
+
+def init(workers, queue):
+    """
+    Starts the worker Threads
+    @param queue: the queue passed to the Worker Threads
+    @param workers: Number of worker Threads
+    """
+    worker_threads = list()
+    for _ in range(workers):
+        worker_threads.append(Worker(queue))
+
